45043c978fb7ae91bfdee656d96735c23eca2b5c
Asher Roberts <asher.roberts98@gmail.com>
2025-01-09 23:48:57 -0800

    I took a breather for the holidays. I'm trying a new project in an unfamiliar language, C#. This time we have basic functionality: click on a date on the calendar to see whether you have brushed your teeth or not on that day. Click the Did-you-brush-your-teeth button two times at first, then once every time after, to change the parity on whether you brushed your teeth that day.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Effects;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Brush_Teeth
+{
+    /// <summary>
+    /// Storing a boolean value for whether the user has brushed their teeth, for every calendar day, stored into a dictionary.
+    /// </summary>
+    public partial class MainWindow : Window {
+        private Dictionary<DateTime, bool> brushingData;
+
+        public MainWindow() 
+        {
+            InitializeComponent();
+            brushingData = new Dictionary<DateTime, bool>();
+            BrushCalendar.SelectedDatesChanged += CalendarChange;
+        }
+
+        private void CalendarChange(object sender, SelectionChangedEventArgs e) 
+        {
+            UpdateBrushingStatus();
+            var selectedDate = BrushCalendar.SelectedDate;
+            //Displaying the date to be sure that clicking the calendar has an effect.
+            if (selectedDate.HasValue)
+            {
+                Today.Text = selectedDate.Value.ToString("MMMM dd, yyyy");
+            }
+            else
+            {
+                Today.Text = "Not a fine day";
+            }
+        }
+
+        private void UpdateBrushingStatus()
+        {
+            var selectedDate = BrushCalendar.SelectedDate;
+            if (selectedDate.HasValue)
+            {
+                if (brushingData.TryGetValue(selectedDate.Value, out bool brushed))
+                {
+                    BrushStatus.Text = brushed ? "Brushed" : "Not Brushed";
+                }
+                else
+                {
+                    BrushStatus.Text = "My Teeth!!";
+                }
+            }
+        }
+
+        private void BrushButton_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedDate = BrushCalendar.SelectedDate;
+            if (selectedDate.HasValue)
+            {
+                if (brushingData.TryGetValue(selectedDate.Value, out bool brushed))
+                {
+                    brushingData[selectedDate.Value] = !brushingData[selectedDate.Value];
+                }
+                else
+                {
+                    brushingData[selectedDate.Value] = true;
+                }
+                UpdateBrushingStatus();
+            }
+        }
+    }
+}
